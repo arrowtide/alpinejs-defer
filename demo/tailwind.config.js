2a740cb743114382779bc001135ec24f1b79318e
Jon Karic <jonkaric@live.co.uk>
2024-08-05 16:58:54 +0100

    Make demo nicer

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
+
+    theme: {
+        extend: {
+            fontFamily: {
+                'default': ['DM Sans', '-apple-system, BlinkMacSystemFont, “Segoe UI”, “Roboto”, “Oxygen”, “Ubuntu”, “Cantarell”, “Fira Sans”, “Droid Sans”, “Helvetica Neue”, sans-serif'],
+            }
+        }
+    },
     content: [
         './*.html',
     ],
